76c95eb19a64e6a93e87b0d5281ff7a5276b5fdb
Yugant Singh <yugantsingh9651@gmail.com>
2026-02-16 15:19:23 +0530

    make create failure api

--- Failure_Archive/Backend/src/app.js
+++ Failure_Archive/Backend/src/app.js
@@ -4,9 +4,10 @@ const authRouter =require("../src/routes/auth.route")
 const cookieParser = require("cookie-parser")
 const failureRouter = require("./routes/failure.routes")
 app.use(express.json())
+app.use(cookieParser())
 app.use("/api/auth",authRouter)  // auth method
 app.use("/api/failure",failureRouter) // /failure method 
-app.use(cookieParser())
+
 
 
 
--- Failure_Archive/Backend/src/controllers/failure.controller.js
+++ Failure_Archive/Backend/src/controllers/failure.controller.js
@@ -1,6 +1,49 @@
 const failureModel = require("../models/failure.model")
+const jwt = require("jsonwebtoken")
+async function createFailureController(req, res) {
 
-async function createFailureController (req,res){
+    try {
+        const { title, description, lesson, tags, isAnonymous, relatableCount } = req.body
+        const token = req.cookies.token
+        console.log(token)
+        if (!token) {
+            return res.status(403).json({
+                message: "token not found"
+            })
+        }
+
+        let decode;
+        try {
+            decode = jwt.verify(token, process.env.JWT_SECRET)
+        }
+        catch (err) {
+            return res.status(403).json({
+                message: "invaild toke! Unauthorized access"
+            })
+        }
+        const userId = decode.id
+        const failure = await failureModel.create({
+            title,
+            description,
+            lesson,
+            tags,
+            isAnonymous,
+            relatableCount,
+            user:userId
+        })
+
+        res.status(201).json({
+            message:"failure created successfuly",
+            failure
+        })
+    }
+
+    catch (err) {
+        return res.status(500).json({
+            message: "Server Error",
+            error: err.message
+        })
+    }
 
 }
 
